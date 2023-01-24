71d17f65447f978253c87d64f0c00548854d5cdd
ajithanalluri <44363166+ajithanalluri@users.noreply.github.com>
2023-01-24 17:46:04 +0530

    Delete utils/src/main/java/org/example directory

--- utils/src/main/java/org/example/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) % 2 == 0;
-    }
-}
